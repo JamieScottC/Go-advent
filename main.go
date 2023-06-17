cfa295446bb555f6291828c3a715de4f4742118e
JamieScottC <scott.jams@gmail.com>
2023-06-17 15:37:31 -0400

    advent dayone

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-  "fmt" 
-  "os"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
 )
 
 func main(){
@@ -11,13 +13,39 @@ func main(){
   //   panic(err)
 
   // }
-  f, err := os.Open("./dayone.txt")
+  filename := "./dayone.txt"
+  f, err := os.Open(filename)
   if (err != nil){
     panic(err)
   }
-  b1 := make([]byte, 6)
+
+  fileinfo, err := os.Stat(filename)
+  b1 := make([]byte, fileinfo.Size())
   n1, err := f.Read(b1)
-  fmt.Print(string(b1[:n1]))
-  
+  fileString := string(b1[:n1])
+
+  var elves []int
+  elves = make([]int, 500)
+  var currentMeal []byte
+  currentElf := 0
+  i := 0
+  var maxCals float64 = 0.0
+
+  for i < len(fileString){
+    if fileString[i] == 10{
+      meal, _ := strconv.Atoi(string(currentMeal))
+      elves[currentElf] += meal
+      currentMeal = []byte{}
+      if i + 1 < len(fileString) && fileString[i + 1] == 10{
+        maxCals = math.Max(float64(maxCals), float64(elves[currentElf]))
+        currentElf++
+      }
+      i += 1
+      continue
+    }
+    currentMeal = append(currentMeal, fileString[i])
+    i += 1
+  }
+  fmt.Print(maxCals)
 }
 
